f8a15fac9424b1d52cc10a490c4eccf49a3033ca
unknown <haykalestu@gmail.com>
2024-05-06 19:35:06 +0700

    Haykal Estu

---
+++ src/components/navbar/LeftNav.vue
@@ -0,0 +1,25 @@
+<template>
+        <v-navigation-drawer
+          expand-on-hover
+          rail
+        >
+          <v-list>
+            <v-list-item
+              prepend-avatar="src/3800_6_08.jpg"
+              subtitle="haykal.estu@widyatama.ac.id"
+              title="Haykal Estu Deanova"
+            ></v-list-item>
+          </v-list>
+  
+          <v-divider></v-divider>
+  
+          <v-list density="compact" nav>
+            <v-list-item prepend-icon="mdi-folder" title="Home" :to="'/home'"></v-list-item>
+            <v-list-item prepend-icon="mdi-account-multiple" title="Dosen" :to="'/dosen-entry'"></v-list-item>
+            <v-list-item prepend-icon="mdi-star" title="Mahasiswa" :to="'/mahasiswa-entry'"></v-list-item>
+            <v-list-item prepend-icon="mdi-star" title="Ruangan" :to="'/ruangan-entry'"></v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+  
+        <v-main style="height: 250px"></v-main>
+  </template>
---
+++ src/views/uts/home.vue
@@ -0,0 +1,9 @@
+<template>
+<h1>WELCOME TO APPLICATION</h1>
+</br>
+<h2>Aplikasi ini dibuat oleh : </h2>
+</br>
+<h4>Nama : Haykal Estu Deanova</h4>
+</br>
+<h4>NPM : 40622100109</h4><br>
+</template>
